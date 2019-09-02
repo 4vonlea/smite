0f2a91cbbe3c58745c85500b073d5a37baa2d993
Muhammad Zaien <muhammad.zaien17@gmail.com>
2019-09-02 15:07:07 +0800

    Ready Register on Site

--- application/controllers/member/Payment.php
+++ application/controllers/member/Payment.php
@@ -128,8 +128,8 @@ class Payment extends MY_Controller
 				$time = time();
 				$custom_expiry = array(
 					'start_time' => date("Y-m-d H:i:s O", $time),
-					'unit' => 'minute',
-					'duration' => 5
+					'unit' => 'day',
+					'duration' => 1
 				);
 
 				$transaction_data = array(
