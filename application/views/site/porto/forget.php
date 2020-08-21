81a9d0769a74a7b7bff271edc38d750acb096a49
untungsb <untungsb@ulm.ac.id>
2020-08-22 07:15:51 +0800

    perbaikan tampilan forget password

--- application/views/site/porto/forget.php
+++ application/views/site/porto/forget.php
@@ -1,12 +1,12 @@
-<section class="page-header page-header-modern bg-color-quaternary page-header-md custom-page-header">
-    <div class="container">
+<section class="page-header page-header-modern bg-color-quaternary page-header-sm custom-page-header">
+    <div class="container container mt-5 pt-5">
         <div class="row mt-3">
             <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
                 <h1>Forget Password</h1>
             </div>
             <div class="col-md-4 order-1 order-md-2 align-self-center">
                 <ul class="breadcrumb d-block text-md-right breadcrumb-light">
-                    <li><a href="<?=base_url('site/home');?>">Home</a></li>
+                    <li><a href="<?=base_url('site/committee');?>">Home</a></li>
                     <li class="active">Forget Password</li>
                 </ul>
             </div>
