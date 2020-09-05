36786130ea10340d69579d0eb8ee37820fd27f6e
Muhammad Zaien <muhammad.zaien17@gmail.com>
2020-09-06 01:16:38 +0800

    Perbaikan

--- application/views/admin/material.php
+++ application/views/admin/material.php
@@ -8,42 +8,7 @@
 	<div class="container-fluid">
 		<div class="header-body">
 			<!-- Card stats -->
-			<div class="row">
-				<div class="col-xl-6 col-lg-6">
-					<div class="card card-stats mb-4 mb-xl-0">
-						<div class="card-body">
-							<div class="row">
-								<div class="col">
-									<h5 class="card-title text-uppercase text-muted mb-0">Total Members</h5>
-									<span class="h2 font-weight-bold mb-0">{{ pagination.total_members }}</span>
-								</div>
-								<div class="col-auto">
-									<div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-										<i class="fas fa-chart-bar"></i>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-6 col-lg-6">
-					<div class="card card-stats mb-4 mb-xl-0">
-						<div class="card-body">
-							<div class="row">
-								<div class="col">
-									<h5 class="card-title text-uppercase text-muted mb-0">Unverified Members</h5>
-									<span class="h2 font-weight-bold mb-0">{{ pagination.total_unverified }}</span>
-								</div>
-								<div class="col-auto">
-									<div class="icon icon-shape bg-warning text-white rounded-circle shadow">
-										<i class="fas fa-chart-pie"></i>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+
 		</div>
 	</div>
 </div>
