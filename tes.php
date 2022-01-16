5e6325451ff8951567d271da1a210dbd0d849db0
Cyber Panel <pandji.winata@ulm.ac.id>
2022-01-16 09:23:59 +0000

    Set Under Maintenance

--- tes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","aomc_smite","Dqs0N4d9f##1jxBl","aomc_smite");
-
-// Check connection
-if ($mysqli->connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-  exit();
-}
