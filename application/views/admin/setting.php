8a7e8c29ecff55cb94cd59d36a3efdfed10029c5
Muhammad Zaien <muhammad.zaien@ulm.ac.id>
2022-01-16 18:16:54 +0800

    kurs usd

--- application/views/admin/setting.php
+++ application/views/admin/setting.php
@@ -508,8 +508,7 @@
 	
 	var banks = <?= $manual; ?>;
 	var emailReceive = "<?= Settings_m::getSetting("email_receive"); ?>";
-	var kurs_usd = <?= $kursUsd = Settings_m::getSetting("kurs_usd") == "" ? 0 : $kursUsd; ?>;
-	console.log(kurs_usd);
+	var kurs_usd = <?= $kursUsd = Settings_m::getSetting("kurs_usd") == "" ? '{using_api:false,value:0}' : $kursUsd; ?>;
 	const toBase64 = file => new Promise((resolve, reject) => {
 		const reader = new FileReader();
 		if (file) {
