7478a4ce854a346c04defca22c18336998e551bd
Muhammad Zaien <muhammad.zaien@ulm.ac.id>
2022-06-09 07:56:00 +0800

    Join event

--- application/views/layouts/gigaland.php
+++ application/views/layouts/gigaland.php
@@ -139,6 +139,11 @@ $theme_path = base_url("themes/gigaland") . "/";
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="offset-4 col-8">
+                        <a href="https://live.aomc-pinbanjarmasin2022.com/login" class="btn-main btn-lg" style="background-color:#00fdff; color:black;">-------------Click here to Join Event-------------</a><br>
+                    </div>
+                </div>
             </div>
     </div>
     </header>
