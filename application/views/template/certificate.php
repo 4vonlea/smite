11cb9c66a6d4a91ff3ae06fa4136b207d6bd757e
Cyber Panel <pandji.winata@ulm.ac.id>
2022-06-19 09:50:11 +0700

    Home modified

--- application/views/template/certificate.php
+++ application/views/template/certificate.php
@@ -17,7 +17,6 @@ header('Content-Type: text/html');
 	}
 	.page-break{
 		page-break-after: always;
-		width: 100%;
 		height: 100%;
 	}
 	.img-fit {
@@ -31,7 +30,7 @@ header('Content-Type: text/html');
   	}
 </style>
 <div class="page-break">
-<img src="<?= $image; ?>" style="width: 100%;position: absolute;top:-1.3%"/>
+<img src="<?= $image; ?>" style="width: 100%;max-height:100%;top:-1.3%"/>
 <?php if(isset($property)) foreach ($property as $i=>$row): ?>
 	<div
 		style="text-align:center;font-size:<?= $row['style']['fontSize']; ?>px ;font-weight:<?= $row['style']['fontWeight']; ?>;position: absolute;z-index: <?=$i;?>;width:<?= $row['style']['width']; ?>%;top:<?= $row['style']['top']; ?>%;left: <?= $row['style']['left']; ?>%">
