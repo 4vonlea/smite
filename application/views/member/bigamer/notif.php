0f34c1d932fe1477813595ebf5f19aa696695b0d
Muhammad Zaien <z4ien.knight@gmail.com>
2022-07-16 17:13:19 +0800

    notif register

--- application/views/member/bigamer/notif.php
+++ application/views/member/bigamer/notif.php
@@ -2,29 +2,29 @@
 /**
  * @var array $participantsCategory
  */
+$theme_path = base_url("themes/bigamer") . "/";
+
 ?>
-<section class="page-header page-header-modern bg-color-quaternary page-header-md custom-page-header">
+<section class="pageheader-section" style="background-image: url(<?= $theme_path; ?>assets/images/pageheader/bg.jpg);">
     <div class="container">
-        <div class="row mt-3">
-            <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
-                <h1>Email Confirmation</h1>
-            </div>
-            <div class="col-md-4 order-1 order-md-2 align-self-center">
-                <ul class="breadcrumb d-block text-md-right breadcrumb-light">
-                    <li><a href="<?= base_url('site/home'); ?>">Home</a></li>
-                    <li class="active">Registration</li>
-                </ul>
-            </div>
+        <div class="section-wrapper text-center text-uppercase">
+            <h2 class="pageheader-title">Email Confirmation</h2>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Registration</li>
+                </ol>
+            </nav>
         </div>
     </div>
 </section>
 
-<section id="app" class="custom-section-padding">
+<section id="app" class="padding-top padding-bottom">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 col-lg-offset-2">
-                <div class="alert alert-success">
-                    <h4><i class="fa fa-info"></i><?=$title;?></h4>
+                <div class="alert btn-purple">
+                    <h4 class="text-dark"><i class="icofont icofont-info-circle"></i><?=$title;?></h4>
                     <p><?=$message;?></p>
                 </div>
             </div>
