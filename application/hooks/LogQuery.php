e98aa15d903619eedba479a19957897f8e6b5101
PC-ELZIEND <muhammad.zaien17@gmail.com>
2023-04-13 22:59:52 +0800

    Prevent hook log query duplicate

--- application/hooks/LogQuery.php
+++ application/hooks/LogQuery.php
@@ -1,28 +1,34 @@
 <?php
 
-class LogQuery {
+class LogQuery
+{
 
-    function write() {
-        $CI = & get_instance();
-        $db = $CI->load->database('',true);
+    function write()
+    {
+        $CI = &get_instance();
+        $db = $CI->load->database('', true);
         $username = $CI->session->user_session['username'] ?? "-";
         $ip = $CI->input->ip_address();
         $postData = $CI->input->post();
         unset($postData['secondPage']);
-        $request = json_encode(array_merge($CI->input->get(),$postData));
+        $request = json_encode(array_merge($CI->input->get(), $postData));
         $controller = $CI->input->server('REQUEST_URI');
+        $tempQuery = [];
         foreach ($CI->db->queries as $key => $query) {
             $date = date('Y-m-d H:i:s');
-            if (!in_array(strtoupper(substr(trim($query),0,6)),["SELECT","SHOW C"])) {
-                $db->insert('log_proses',array(
-                    'controller'=>$controller ?? "",
-                    'username'=>$username.":".$ip,
-                    'request'=>$request,
-                    'query'=>$query,
-                    'date'=>$date,
-                ));
+            $hashQuery = sha1($query);
+            if (!in_array($hashQuery, $tempQuery)) {
+                $tempQuery[] = $hashQuery;
+                if (!in_array(strtoupper(substr(trim($query), 0, 6)), ["SELECT", "SHOW C"])) {
+                    $db->insert('log_proses', array(
+                        'controller' => $controller ?? "",
+                        'username' => $username . ":" . $ip,
+                        'request' => $request,
+                        'query' => $query,
+                        'date' => $date,
+                    ));
+                }
             }
         }
- 
     }
 }
